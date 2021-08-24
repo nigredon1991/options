3ea9a053afe3bea12f5d0011b6148310f353d989
nigredon1991 <nigredon1991@gmail.com>
2021-08-24 18:42:39 +0500

    Add dart

--- scripts/s_l_a_c_k_hide_unread_blue.sh
+++ scripts/s_l_a_c_k_hide_unread_blue.sh
@@ -1,5 +1,6 @@
 # npm install -g asar
-mkdir ~/tmp/slack
+set -x
+mkdir -p ~/tmp/slack
 asar extract /usr/lib/slack/resources/app.asar ~/tmp/slack
 sed -i 's/unreads>0/unreads<0/g' ~/tmp/slack/dist/main.*.js
 sudo rm /usr/lib/slack/resources/app.asar
